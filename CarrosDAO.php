8eea654296905243b89393a7cdb6269661311764
douglasrosa16 <42454362+douglasrosa16@users.noreply.github.com>
2019-11-02 19:03:56 -0400

    Novas implementações

---
+++ CarrosDAO.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once('db.php');
+
+class CarrosDAO extends db{
+
+    public function __construct(){
+        
+    }
+
+    //Mostrar as consultas
+    public function select(){
+
+    }
+
+    //Deletar os arquivos
+    public function delete(){
+
+    }
+
+    //Inserir novos dados
+    public function insert(){
+
+    }
+
+    //Atualizar informações
+    public function update(){
+
+    }
+
+    public function __destruct(){
+        foreach($this as $key => $value){
+            unset($this->key);
+        }
+        foreach(array_keys(get_defined_vars)  as $var){
+            unset(${"$var"});    
+        } 
+        unset($var);
+   }
+
+
+
+
+
+
+}
+
+
+
+?>
--- db.php
+++ db.php
@@ -1,11 +1,17 @@
 <?php
 
 //Conexão com Banco de Dados
+$database = "locadora";
+$host = "localhost";
+$user = "root";
+$password = "";
 
+$conn = new mysqli($host, $user, $password, $database);
 
-
-
-
-
+if(!$conn->connect_errno){
+    echo "Conectado com Sucesso!";
+}else{
+    die("Falha na conexão: " . $conn->connect_error);
+}
 
 ?>
