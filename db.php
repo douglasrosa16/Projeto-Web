62d8eae92cd43baed29df3155ca1efcc1643f755
douglasrosa16 <42454362+douglasrosa16@users.noreply.github.com>
2019-12-02 12:55:35 -0400

    estudando

--- db.php
+++ db.php
@@ -1,25 +1,69 @@
+
 <?php
 
-//Conexão com Banco de Dados
-$database = "locadora";
-$host = "localhost";
-$user = "root";
-$password = "";
+Class Db {
+  private $host;
+  private $user;
+  private $passwd;
+  private $db;
+  private $con;
+  private $connected;
 
-//Abrir conexão
-public function DBConnect(){
-    $conn = new mysqli($host, $user, $password, $database);
+  public function __construct($host, $user, $passwd, $db)
+  {
+    $this->host = $host;
+    $this->user = $user;
+    $this->passwd = $passwd;
+    $this->db = $db;
+    $this->connected = false;
+  }
 
-    if(!$conn->connect_errno){
-        echo "Conectado com Sucesso!";
-    }else{
-        die("Falha na conexão: " . $conn->connect_error);
-    }    
+  public function connect() {
+    $this->con = new mysqli($this->host, $this->user, 
+                            $this->passwd, $this->db);
+    if (! $this->con->connect_errno) {
+      $this->connected = true;
+      return true;
+    }
+    return false;
+  }
+
+  public function isConnected() {
+    return $this->connected;
+  }
+
+  public function exec($sql) {
+    $res = null;
+    if ($this->connected) {
+      $res = $this->con->query($sql);
+    }
+    return $res;
+  }
+
+  public function prepare($sql) {
+    $stmt = null;
+    if ($this->connected) {
+      $stmt = $this->con->prepare($sql);
+    }
+    return $stmt;
+  }
+
+  public function getLastID() {
+    $id = null;
+    if ($this->connected) {
+      $id = $this->con->insert_id;
+    }
+    return $id;
+  }
+
+  public function __destruct() {
+    if ($this->connected) {
+      $this->con->close();
+      $this->connected = false;
+    }
+  }
+
 }
 
-//Fechar Conexão
-public function DBClose($conn){
-    mysqli_close($conn);
-}
 
-?>
+
