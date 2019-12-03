30c98dc09b4c3c93ec7d3d823c773b9f12f39081
douglasrosa16 <42454362+douglasrosa16@users.noreply.github.com>
2019-12-03 12:45:41 -0400

    trabalho

--- CarrosDAO.php
+++ CarrosDAO.php
@@ -1,31 +1,103 @@
 <?php
 
 require_once('db.php');
-
+require_once('carro.php');
 class CarrosDAO extends db{
 
     public function __construct(){
         
     }
 
-    //Mostrar as consultas
-    public function select(){
-
+    public function getCarros() {
+        if ($this->connection->isConnected()) {
+            $sql="SELECT carro_id, modelo, ano, placa, marca_id from carros";
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute();
+            $stmt->bind_result($carro_id, $modelo, $ano, $placa, $marca_id);
+            $res = $stmt->store_result();
+            $carros = [];
+            if ($stmt->num_rows > 0) {
+                while ($stmt->fetch()) {
+                    $carros[] = new Carro($carro_id, $modelo, $ano, $placa, $marca_id);
+                }
+            }
+            $stmt->close();
+            return $carros;
+        }
+        return [];
     }
 
-    //Deletar os arquivos
-    public function delete(){
-
+    public function insereCarro(Carro $carro) {
+        if ($this->connection->isConnected()) {
+            $sql = "INSERT INTO carros (modelo, ano, placa) VALUES(?,?,?)";
+            $stmt = $this->connection->prepare($sql);
+            if (isset($stmt)) {
+                $modelo = $carro->getModelo()
+                $ano    = $carro->getAno();
+                $placa  = $carro->getPlaca();
+                $id_marca = $carro->getMarca_id();
+                $stmt->bind_param('sss',$modelo, $ano, $placa);
+                if ($stmt->execute()) {
+                    $lastId = $this->connection->getLastID();
+                    $carro->setIdCarro($lastId);
+                    $stmt->close();
+                    return $carro;
+                }
+                $stmt->close();
+            }
+        }
+        return null;
     }
 
-    //Inserir novos dados
-    public function insert(){
-
+    public function getCarroByID($id) {
+        if ($this->connection->isConnected()) {
+            $sql = "SELECT carro_id, modelo, ano, placa, marca_id from carros where carro_id = ?";
+            $stmt = $this->connection->prepare($sql);
+            $stmt->bind_param('i', $id);
+            if ($stmt->execute()) {
+                $stmt->bind_result($carro_id, $modelo, $ano, $placa, $marca_id);
+                $stmt->store_result();
+                if ($stmt->num_rows > 0) {
+                    if ($stmt->fetch()) {
+                        $stmt->close();
+                        return new Carro ($carro_id, $modelo, $ano, $placa, $marca_id);
+                    }
+                }
+            }
+            $stmt->close();
+        }
+        return null;
     }
 
-    //Atualizar informações
-    public function update(){
+    public function apagarCarro(Carro $carro) {
+        if ($this->connection->isConnected()) {
+            $sql = "DELETE FROM carros WHERE carro_id = ?";
+            $stmt = $this->connection->prepare($sql);
+            if (isset($stmt)) {
+                $id = $carro->getId();
+                $stmt->bind_param('i', $id);
+                $res = $stmt->execute();
+                $stmt->close();
+            }
+            return $res;
+        }
+        return false;
+    }
 
+    public function salvarCarro(Carro $carro) {
+        if ($this->connection->isConnected()) {
+            $sql = "UPDATE carros SET modelo=? WHERE carro_id=?";
+            $stmt = $this->connection->prepare($sql);
+            if (isset($stmt)) {
+                $carro_id = $carro->getId();
+                $modelo = $carro->getModelo();
+                $stmt->bind_param('si',$modelo, $carro_id);
+                $res = $stmt->execute();
+                $stmt->close();
+                return $res;
+            }
+        }
+        return false;
     }
 
     //Libera dados da memória
@@ -39,13 +111,6 @@ class CarrosDAO extends db{
         unset($var);
    }
 
-
-
-
-
-
 }
-
-
-
-?>
+  
+   ?>
---
+++ carro.php
@@ -0,0 +1,62 @@
+<?php
+
+    class Carro{
+
+      private $carro_id;
+      private $modelo;
+      private $ano;
+      private $placa;
+      private $marca_id;
+
+        public function __construct($carro_id=null, $modelo=null, $ano=null, $placa=null, $marca_id=null){
+            $this->carro_id = $carro_id;
+            $this->modelo = $modelo;
+            $this->ano = $ano;
+            $this->placa = $placa;
+            $this->marca_id = $marca_id;
+        }
+
+        public function setId($carro_id){
+            $this->carro_id = $carro_id;
+        }
+
+        public function getId(){
+            return $this->carro_id;
+        }
+
+        public function setModelo($modelo){
+            $this->modelo = $modelo;
+        }
+
+        public function getModelo(){
+            return $this->modelo;
+        }
+
+        public function setAno($ano){
+            $this->ano = $ano;
+        }
+
+        public function getAno(){
+            return $this->ano;
+        }
+
+        public function setPlaca($placa){
+            $this->placa = $placa;
+        }
+
+        public function getPlaca(){
+            return $this->placa;
+        }
+
+        public function setMarca_id($marca_id){
+            $this->marca_id = $marca_id;
+        }
+
+        public function getMarca_id(){
+            return $this->marca_id;
+        }
+    }
+
+
+
+?>
